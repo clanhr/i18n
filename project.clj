f6681d357ebebe2f66c9d7c6c9c887780aca7442
Marcos <marcoslamuria@gmail.com>
2016-01-12 12:21:32 +0000

    add keys hours and days

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/i18n "1.2.0"
+(defproject clanhr/i18n "1.4.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
   :dependencies [[org.clojure/clojure "1.8.0-RC2"]
