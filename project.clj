ba900f042af56d8abd09fa1a548655512c6e9b62
Pedro Santos <donbonifacio@gmail.com>
2016-04-11 15:22:44 +0100

    Sync tokens

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clanhr/i18n "1.11.0"
+(defproject clanhr/i18n "1.12.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
-  :dependencies [[org.clojure/clojure "1.8.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]]
 
    :aliases {"sync"  ["run" "-m" "clanhr.i18n.sync/sync-data"]}
