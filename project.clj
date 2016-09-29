34ede496e2849da87b6fd290efa943103331d6da
Marcos Lamúria <marcoslamuria@gmail.com>
2016-09-29 11:46:17 +0100

    add missing translations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/i18n "2.0.0"
+(defproject clanhr/i18n "2.1.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
   :dependencies [[org.clojure/clojure "1.8.0"]
