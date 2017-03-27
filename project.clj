3e490432bd43ab016e084c180bd9f7c1c81f6f88
claudiaalves <claudia.alves@rupeal.com>
2017-03-27 20:59:29 +0100

    Add kms report translations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/i18n "2.25.0"
+(defproject clanhr/i18n "2.26.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
   :dependencies [[org.clojure/clojure "1.8.0"]
