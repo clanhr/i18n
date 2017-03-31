cbab31d280f957f129cabbc2b3926bb2b95f2737
claudiaalves <claudia.alves@rupeal.com>
2017-03-31 14:52:48 +0100

    Fix the expense state canceled translations

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/i18n "2.26.0"
+(defproject clanhr/i18n "2.27.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
   :dependencies [[org.clojure/clojure "1.8.0"]
