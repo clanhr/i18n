ab8d03be017060372e4f557ef85fadc81fd7ef37
Paulo Silva <paulosilva86@gmail.com>
2019-03-28 11:17:45 +0000

    Add new expense types
    Why:
    
    * So we can display the name of the new expense types.
    
    This change addresses the need by:
    
    * Adding 'pt' and 'en' translations for 'commissions' and 'allowances'.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/i18n "2.32.0"
+(defproject clanhr/i18n "2.33.0"
   :description "ClanHR's i18n support"
   :url "https://github.com/clanhr/i18n"
   :dependencies [[org.clojure/clojure "1.8.0"]
